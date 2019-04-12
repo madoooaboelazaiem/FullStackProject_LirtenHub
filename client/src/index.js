94596384cd91c7081f5a74dd970601cc7d7d3cf8
madoooaboelazaiem <Madooo1998@hotmail.com>
2019-04-12 20:09:57 +0200

    with react

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import Home from './Home';
 import * as serviceWorker from './serviceWorker';
