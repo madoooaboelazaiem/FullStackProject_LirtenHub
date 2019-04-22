34f2d9e2864d335762439c846270dbf8e33943e1
madoooaboelazaiem <Madooo1998@hotmail.com>
2019-04-22 23:53:40 +0200

    done

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -9,6 +9,8 @@ import 'tachyons'
   render() {
     const {isLoggedIn,loggedUser,users} = this.props;
     const x = loggedUser.User_Category
+    if(x.User_Category=="Admin"){
+
     return(
       <div className = 'haw'>
                 <nav className="bt hover-bg-light-gray tc mw7 center mt4">
@@ -50,7 +52,138 @@ import 'tachyons'
                 
        
         </div>
-  )
+  )}
+  else if(x.User_Category=="Partner"){
+    return(
+      <div className = 'haw'>
+      <nav className="bt hover-bg-light-gray tc mw7 center mt4">
+        <div className="dropdown"> 
+            <a className="f6 f5-l link bg-animate black-80 hover-bg-green dib pa3 ph4-l" >Projects</a>
+            <div className="dropdown-content bg-light-green ">
+              <Link to="/Projects/">All Projects</ Link>
+              <Link to="/approvedP/">Approved Projects</ Link>
+              <Link to="/notapprovedP/">Not Approved Projects</ Link>
+              <Link to="/Project_Requests">Project Requests</Link>
+            </div>
+          </div>
+          <Link className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" >Test Remove dropdowns if not neede ... 
+          ps remove homepage if not needed
+          </Link>
+
+          
+    
+        <div className="dropdown">
+          <a className="f6 f5-l link bg-animate black-80 hover-bg-gray dib pa3 ph4-l" >Locations</a>
+          <div className="dropdown-content bg-beige ">
+            <a href="/#">s</a>
+            <a href="/#">2</a>
+            <a href="/#">Cancel Reservation</a>
+            <a href="/#">Add Locations</a>
+            <a href="/#">Add Room</a>
+            <a href="/#">Edit Room</a>
+          </div>
+        </div>
+    
+
+
+       
+       <div className="dropdown"></div>      
+
+
+      </nav>
+
+      
+
+</div>
+    )}
+    else if(x.User_Category=="Consulting_Agent"){
+      return(
+        <div className = 'haw'>
+        <nav className="bt hover-bg-light-gray tc mw7 center mt4">
+          <div className="dropdown"> 
+              <a className="f6 f5-l link bg-animate black-80 hover-bg-green dib pa3 ph4-l" >Projects</a>
+              <div className="dropdown-content bg-light-green ">
+                <Link to="/Projects/">All Projects</ Link>
+                <Link to="/approvedP/">Approved Projects</ Link>
+                <Link to="/notapprovedP/">Not Approved Projects</ Link>
+                <Link to="/Project_Requests">Project Requests</Link>
+              </div>
+            </div>
+            <Link className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" >Test Remove dropdowns if not neede ... 
+            ps remove homepage if not needed
+            </Link>
+
+            
+      
+          <div className="dropdown">
+            <a className="f6 f5-l link bg-animate black-80 hover-bg-gray dib pa3 ph4-l" >Locations</a>
+            <div className="dropdown-content bg-beige ">
+              <a href="/#">s</a>
+              <a href="/#">2</a>
+              <a href="/#">Cancel Reservation</a>
+              <a href="/#">Add Locations</a>
+              <a href="/#">Add Room</a>
+              <a href="/#">Edit Room</a>
+            </div>
+          </div>
+      
+
+
+         
+         <div className="dropdown"></div>      
+
+
+        </nav>
+
+        
+
+</div>
+      )}
+else{
+          return(
+            <div className = 'haw'>
+            <nav className="bt hover-bg-light-gray tc mw7 center mt4">
+              <div className="dropdown"> 
+                  <a className="f6 f5-l link bg-animate black-80 hover-bg-green dib pa3 ph4-l" >Projects</a>
+                  <div className="dropdown-content bg-light-green ">
+                    <Link to="/Projects/">All Projects</ Link>
+                    <Link to="/approvedP/">Approved Projects</ Link>
+                    <Link to="/notapprovedP/">Not Approved Projects</ Link>
+                    <Link to="/Project_Requests">Project Requests</Link>
+                  </div>
+                </div>
+                <Link className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" >Test Remove dropdowns if not neede ... 
+                ps remove homepage if not needed
+                </Link>
+
+                
+          
+              <div className="dropdown">
+                <a className="f6 f5-l link bg-animate black-80 hover-bg-gray dib pa3 ph4-l" >Locations</a>
+                <div className="dropdown-content bg-beige ">
+                  <a href="/#">s</a>
+                  <a href="/#">2</a>
+                  <a href="/#">Cancel Reservation</a>
+                  <a href="/#">Add Locations</a>
+                  <a href="/#">Add Room</a>
+                  <a href="/#">Edit Room</a>
+                </div>
+              </div>
+          
+
+
+             
+             <div className="dropdown"></div>      
+
+
+            </nav>
+
+            
+   
+    </div>
+        ) 
+
+  }
     }   
   }
 function mapStateToProps(state) {
