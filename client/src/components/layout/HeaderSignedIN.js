9daf5ffb969aa983997b8ced06d2a15d93f8c212
unknown <seifmohamed728@gmail.com>
2019-04-22 23:12:14 +0200

    headersigned in changed admin working

--- client/src/components/layout/HeaderSignedIN.js
+++ client/src/components/layout/HeaderSignedIN.js
@@ -64,85 +64,95 @@ axios.put('Https://Lirtenhubtest.herokuapp.com/api/users/Logout/'+(this.props.lo
       if(this.props.loggedUser.User_Category=="Admin"){
         return(
           <div>
-        
-        
-          <body >
-          
-          
-          <div className="d-flex toggled" id="wrapper">
 
-<div className="bg-light border-right" id="sidebar-wrapper">
-  <div className="sidebar-heading"> <img src="https://img.icons8.com/color/48/000000/superman.png"/>    </div>
-  <div className="list-group list-group-flush">
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/Locations">Locations</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/CoworkingLoc">Locations</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/AcceptRejectReservation">Reservations</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/Reserve">Add Reservation</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/DeleteReservations">Cancel Reservation</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/AddLocations">Add Locations</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/LocationRoom">Add Room</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/Rooms">Edit Room</Link>
-  <Link className="list-group-item list-group-item-action white-80 bg-light" to="/projects" >All Projects</Link>
 
-  </div>
-</div>
 
-          
-              <div id="page-content-wrapper">
-          
-                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                  <button class="btn btn-primary" id="menu-toggle" onClick={this._toggleDiv}>LirtenHub</button>
-          
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-          
-                  <div class="collapse navbar-collapse white-80" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0 ">
-                    
-                      <li class="nav-item">
 
-                      <Link   class="nav-link hover-bg-light-blue" to={"/SingleValidateUser"} >Validate Users</Link>
-                      </li> 
-                      <li class="nav-item">
-                      <Link   class="nav-link hover-bg-light-blue" to={"/SingleUpdate"} >AcceptorReject Updates</Link>
+  
 
-              </li> 
-                      <li class="nav-item">
-                      <Link   class="nav-link hover-bg-light-gray" to={"/AcceptrejectSingleSkill"} >AcceptorReject Skills</Link>
-                      </li> 
-                       <li class="nav-item">
-                       <Link  class="nav-link hover-bg-light-gray"  to={"/Profile/"+x.id} >MyProfile</Link>       
-                            </li> 
-                      <li class="nav-item dropdown white ">
-                        <a class="nav-link dropdown-toggle hover-bg-light-gray " href="#" id="navbarDropdown" role="button " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Account
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                          
-                        <form onSubmit={this.onSubmit.bind(this)}>
-                                <button class="dropdown-item black-80" type="submit">Logout</button></form>
-          
-                          <a class="dropdown-item black-80" href="/">ChangeAccount</a>
-                          <div class="dropdown-divider white-80"></div>
-                          <a class="dropdown-item black-80" href="/">About</a>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                  
-                </nav>
-          
-               
-              </div>
-          
-            </div>
-          
-          
-          
-          </body>
+  
+        <div className="d-flex toggled" id="wrapper">
+    
+        <div className="bg-light border-right" id="sidebar-wrapper">
+          <div className="sidebar-heading"> <img src='https://img.icons8.com/color/48/000000/batman-old.png'/>    </div>
+          <div className="list-group list-group-flush">
+          {/* <Link className="list-group-item list-group-item-action white-80 bg-light" to="/CoworkingLoc">MyLocations</Link> */}
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/Locations/notEdit">All Locations</Link>
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/AcceptRejectReservation">Reservations</Link>
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/Reserve">Add Reservation</Link>
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/DeleteReservations">Cancel Reservation</Link>
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/AddLocations">Add Locations</Link>
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/LocationRoom">Add Room</Link>
+          {/* <Link className="list-group-item list-group-item-action white-80 bg-light" to="/Rooms">Edit Room</Link> */}
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/Calendar">Calendar</Link>
+          <Link className="list-group-item list-group-item-action white-80 bg-light" to="/projects" >All Projects</Link>
+    
           </div>
+          
+        </div>
+        
+    
+      
+          <div id="page-content-wrapper">
+      
+            <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+              <button className="btn btn-primary" id="menu-toggle" onClick={this._toggleDiv}>LirtenHub</button>
+      
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+      
+              <div className="collapse navbar-collapse white-80" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto mt-2 mt-lg-0 ">
+                  <li className="nav-item active white-80">
+                    {/* <a class="nav-link hover-bg-light-gray" href="#">Home <span class="sr-only">(current)</span></a> */}
+                  </li>
+                  <li class="nav-item">
+                     
+                     <Link   class="nav-link hover-bg-light-blue" to={"/SingleValidateUser"} >Validate Users</Link>
+                     </li> 
+                     <li class="nav-item">
+                     <Link   class="nav-link hover-bg-light-blue" to={"/SingleUpdate"} >AcceptorReject Updates</Link>
+
+             </li> 
+                     <li class="nav-item">
+                     <Link   class="nav-link hover-bg-light-gray" to={"/AcceptrejectSingleSkill"} >AcceptorReject Skills</Link>
+                     </li> 
+                      <li class="nav-item">
+                      <Link  class="nav-link hover-bg-light-gray"  to={"/Profile/"+x.id} >MyProfile</Link>       
+                           </li> 
+                  <li className="nav-item dropdown white ">
+                    <a className="nav-link dropdown-toggle hover-bg-light-gray " href="#" id="navbarDropdown" role="button " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Account
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      
+                    <form onSubmit={this.onSubmit.bind(this)}>
+                            <button className="dropdown-item black-80" type="submit">Logout</button></form>
+                           
+                      <Link className="dropdown-item black-80"to = '/SignIn' onClick={this.onSubmit.bind(this)}>ChangeAccount</Link>
+                      <div className="dropdown-divider white-80"></div>
+                      <a className="dropdown-item black-80" href="/">About</a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              
+            </nav>
+            {this.props.children}
+    
+           
+          </div>
+      
+        </div>
+      
+      
+      
+      </div>
+        
         )
+
+         
       }
 return(
     <div>
